d3af6790b2b692e451b158d2af87d1c7c96b9e01
fukumotomasashi <cvb25362@gmail.com>
2019-05-23 23:56:34 +0900

    Create new function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,17 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    pramas.premit(:name, :image, :text)
+  end
 end
